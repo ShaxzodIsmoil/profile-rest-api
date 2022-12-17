343ae0fa76b5da374104a05ddab6028d6c3ca2b0
shaxzod16 <shaxzodrafiqov@gmail.com>
2022-12-17 21:38:41 +0500

    Add post and get api method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,28 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import HelloSerializer
 
 
 class HelloView(APIView):
     """Test API View"""
-    def get(self, request, format=None):
+    serializers_class = HelloSerializer
+
+    def get(self, request):
         an_apiview = [
             'Hello',
             'Salom'
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
