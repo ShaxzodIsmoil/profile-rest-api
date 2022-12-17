38989d5fdd19514647f54334b357d92ef02a474f
shaxzod16 <shaxzodrafiqov@gmail.com>
2022-12-17 21:55:25 +0500

    add viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import HelloView
+from django.urls import path, include
+from .views import HelloView, HelloViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello', HelloViewSet, basename='hello-viewset')
 
 
 urlpatterns = [
-    path('profile-api/', HelloView.as_view())
+    path('profile-api/', HelloView.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ViewSet
 
 from .serializers import HelloSerializer
 
@@ -25,4 +26,28 @@ class HelloView(APIView):
             return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HelloViewSet(ViewSet):
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        an_apiview = [
+            'Hello',
+            'Salom'
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
